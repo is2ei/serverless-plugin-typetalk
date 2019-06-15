058bbc713e17faa75931523dafc59c715c42ab7b
Issei Horie <is2ei.horie@gmail.com>
2019-06-15 20:35:57 +0900

    Added files

---
+++ index.test.js
@@ -0,0 +1,7 @@
+const ServerlessTypetalkPlugin = require('./index');
+
+describe('serverless typetalk plugin', () => {
+  test('constructor function', () => {
+    expect(typeof ServerlessTypetalkPlugin).toBe('function');
+  })
+})
