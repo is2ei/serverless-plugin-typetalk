d62c589f62cf69c89900f730dacce51b4f58aceb
Issei Horie <is2ei.horie@gmail.com>
2019-06-15 22:58:45 +0900

    Send post message on remove event

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 'use strict';
 
+const Typetalk = require('typetalk');
+const repositoryUrl = "https://github.com/is2ei/serverless-plugin-typetalk-example";
+
+function hasRequiredProperties(service) {
+  if (!service.custom) {
+    return false;
+  }
+  if (!service.custom.typetalk) {
+    return false;
+  }
+  if (!service.custom.typetalk.topicId) {
+    return false;
+  }
+  if (!service.custom.typetalk.token) {
+    return false;
+  }
+
+  return true;
+}
+
+
 class TypetalkServerlessPlugin {
   constructor(serverless, options) {
     this.serverless = serverless;
     this.options = options;
 
-    if (!this.serverless.service.custom || !this.serverless.service.custom.typetalk) {
-      throw new Error('ServerlessTypetalkPlugin requires options');
+    if (!hasRequiredProperties(this.serverless.service)) {
+      throw new Error(`ServerlessTypetalkPlugin requires options. see ${repositoryUrl}`);
     }
+
+    this.typetalk = new Typetalk.Client({
+      token: this.serverless.service.custom.typetalk.token
+    });
 
     this.hooks = {
       'before:deploy:resources': this.beforeDeployResources.bind(this),
@@ -27,6 +52,9 @@ class TypetalkServerlessPlugin {
   remove() {
     this.serverless.cli.log('[Serverless Typetalk Plugin Test] remove()');
     console.log('[Serverless Typetalk Plugin Test] remove()');
+    const message = ':bomb: Removed Serverless service and all resources'
+    const id = this.serverless.service.custom.typetalk.topicId
+    this.typetalk.postMessage({message}, {id});
   }
 }
 
